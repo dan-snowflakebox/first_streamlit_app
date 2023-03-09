3afa90233f265baac90ffb444346bca80adcfced
dan-snowflakebox <127366565+dan-snowflakebox@users.noreply.github.com>
2023-03-09 17:05:02 +0800

    Update streamlit_app.py
    Fixed Typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,5 @@ streamlit.title("My Mother New Healthy Dinner");
 streamlit.header("Breakfast Menu");
 streamlit.text("Omega 3 and Blueberry Oatmeal");
 streamlit.text("Kale, Spinach and Rocket Smoothie");
-streamit.text("Hard-Boiled Free Range-Egg");
+streamlit.text("Hard-Boiled Free Range-Egg");
 
