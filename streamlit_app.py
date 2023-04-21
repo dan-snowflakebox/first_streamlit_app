adcc262228852996d1e4a840315182fc4a0c432f
dan-snowflakebox <127366565+dan-snowflakebox@users.noreply.github.com>
2023-04-21 19:34:17 +0800

    Add Text input

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,6 +23,8 @@ fruit_to_show = my_fruit_list.loc[fruits_selected];
 # Display the table in the page
 streamlit.dataframe(fruit_to_show);
 
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
 streamlit.header("Fruityvice Fruit Advice!");
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json());
